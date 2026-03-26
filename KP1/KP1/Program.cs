c7e6d0bf0e1ca80a2b10c0bfd1d5757bf81303f0
DESKTOP-FPDAH3L\MSI <lavrikv14@gmail.com>
2026-03-26 10:34:43 +0200

    Add KR1

---
+++ KP1/KP1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ModularWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Виклик першого завдання
+            Task1.Execute();
+
+            // Виклик другого завдання
+            Task2.Execute();
+
+            Console.WriteLine("\nНатисніть будь-яку клавішу для завершення...");
+            Console.ReadKey();
+        }
+    }
+}
